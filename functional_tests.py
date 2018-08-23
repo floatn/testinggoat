873c7373473175fccead70fddedbc2ec68431947
float2 <nicholas.cheredov@gmail.com>
2018-08-23 18:44:02 +0300

    Extend FT: unique page for every user.

--- functional_tests.py
+++ functional_tests.py
@@ -8,6 +8,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        #self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
@@ -17,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def est_can_start_a_list_for_one_user(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -42,6 +43,39 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get('http://localhost:8000/')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        current_url = self.browser.current_url
+        last_url = current_url
+        self.assertRegex(current_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get('http://localhost:8000')
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        current_url = self.browser.current_url
+        self.assertRegex(current_url, '/lists/.+')
+        self.assertNotEqual(current_url, last_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
