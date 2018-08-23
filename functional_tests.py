f7cb9c5fb5d2ca0ed10cac8bb20b3b685eaf48bd
float2 <nicholas.cheredov@gmail.com>
2018-08-23 15:25:40 +0300

    Enable admin. Redirect after POST, and show all items im template.

--- functional_tests.py
+++ functional_tests.py
@@ -31,14 +31,14 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peackock feathers')
+        time.sleep(2)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
 
-        self.check_for_row_in_list_table('1: Buy peackock feathers')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
