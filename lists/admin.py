e2069494ca91d816ed8163005458f7481a93936c
float2 <nicholas.cheredov@gmail.com>
2018-08-24 17:37:02 +0300

    Add: Item object. Remove: redundant HomePageTest functionality.

--- lists/admin.py
+++ lists/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from lists.models import Item
+from lists.models import Item, List
 
 
+admin.site.register(List)
 admin.site.register(Item)
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -11,18 +11,6 @@ class HomePageTest(TestCase):
         response = self.client.get('')
         #self.assertTemplateUsed(response, 'lists/home.html')
 
-    def test_can_save_a_POST_request(self):
-        self.client.post('', data={'item_text': 'A new list item'})
-
-        #self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
     def test_only_saves_items_when_necessary(self):
         response = self.client.get('')
         self.assertEqual(Item.objects.count(), 0, f'Objects found:\n{Item.objects.count()}.')
@@ -30,8 +18,9 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -42,16 +31,24 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'lists/list.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -59,7 +56,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class NewListTest(TestCase):
 
