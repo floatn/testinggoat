af5899e2c84e3397d182bb5032831316acbd490b
float2 <nicholas.cheredov@gmail.com>
2018-08-25 14:15:23 +0300

    Modify: list.html template and views.

--- lists/views.py
+++ lists/views.py
@@ -6,11 +6,16 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'lists/home.html')
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'lists/list.html', {'items': items})
+def view_list(request, list_id):
+    current_list = List.objects.get(id=list_id)
+    return render(request, 'lists/list.html', {'list': current_list})
 
 def new_list(request):
-    list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    new_list = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=new_list)
+    return redirect(f'/lists/{new_list.id}/')
+
+def add_item(request, list_id):
+    current_list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=current_list)
+    return redirect(f'/lists/{current_list.id}/')
