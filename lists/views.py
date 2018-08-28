3be7392034bc90fda51a6c904e5771e6ff3cea91
float2 <nicholas.cheredov@gmail.com>
2018-08-28 17:01:01 +0300

    Add: bootstrap support.

--- lists/views.py
+++ lists/views.py
@@ -6,16 +6,16 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'lists/home.html')
 
+def add_item(request, list_id):
+    current_list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=current_list)
+    return redirect(f'lists/{current_list.id}/')
+
 def view_list(request, list_id):
     current_list = List.objects.get(id=list_id)
     return render(request, 'lists/list.html', {'list': current_list})
 
 def new_list(request):
-    new_list = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=new_list)
-    return redirect(f'/lists/{new_list.id}/')
-
-def add_item(request, list_id):
-    current_list = List.objects.get(id=list_id)
+    current_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=current_list)
-    return redirect(f'/lists/{current_list.id}/')
+    return redirect(f'lists/{current_list.id}/')
